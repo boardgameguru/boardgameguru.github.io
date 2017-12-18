dd10333b33f0b51754bb3bba6e0ceee8923e4130
boardgameguru <thomas.spencer416@gmail.com>
2017-12-18 16:59:09 -0500

    Added JavaScript to change the class names of the animated-text child elements every 2 seconds. Commented out the setInterval while continuing to work on the remaining Javascript.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function changeText() {
+    var num;
+    var elems = document.getElementsByClassName("animated-text")[0].children
+
+    for(var i = 0; i < elems.length; i++ ) {
+        if (elems[i].className === "is-active") {
+            elems[i].className = "is-hidden"
+            if (i + 1 > elems.length - 1) {
+                num = 0
+            } else {
+                num = i + 1
+            }
+        };
+    };
+
+    elems[num].className = "is-active";
+}
+
+function typeText() {
+
+}
+
+function highlightText() {
+
+}
+
+function deleteText() {
+
+}
+
+//setInterval(function() {changeText()}, 2000)
