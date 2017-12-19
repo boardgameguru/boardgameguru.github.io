fafce0f97058c1abf88670794fd76b49718100ee
boardgameguru <thomas.spencer416@gmail.com>
2017-12-19 13:19:56 -0500

    Added highlightText function to my animated text feature to mimic a user highlighting the text before deletion. Also created animateText function to  reapply the setTimeout delays in my setInterval.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
 function changeText() {
-    var num;
-    var elems = document.getElementsByClassName("animated-text")[0].children
+    var num = 0;
+    var elems = document.getElementsByClassName("animated-text")[0].children;
 
-    for(var i = 0; i < elems.length; i++ ) {
-        if (elems[i].className === "is-active") {
+    for (var i = 0; i < elems.length; i++ ) {
+        if (elems[i].className === "is-active highlighted") {
             elems[i].className = "is-hidden"
             if (i + 1 > elems.length - 1) {
                 num = 0
             } else {
                 num = i + 1
-            }
+            };
         };
     };
-
     elems[num].className = "is-active";
-}
+};
 
 function typeText() {
 
 }
 
 function highlightText() {
+    var active = document.getElementsByClassName("is-active")
+    active[0].classList.add("highlighted")
+};
 
+
+function animateText () {
+    setTimeout(highlightText, 2500);
+    setTimeout(changeText, 3000)
 }
 
-
-//setInterval(function() {changeText()}, 3000)
+animateText();
+setInterval(animateText, 3000);
